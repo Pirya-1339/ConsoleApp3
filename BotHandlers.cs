3867a104d68c699945bb7418e3bb78d509dc60cf
User <User@DESKTOP-6QAC1CU>
2022-07-11 19:42:51 +0300

    Добавьте файлы проекта.

---
+++ BotHandlers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+
+namespace ConsoleApp3
+{
+    internal static class BotHandlers
+    {
+        public async static Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+
+            if (update.Message is not { } message)
+                return;
+
+            if (message.Text is not { } messageText)
+                return;
+            var chatId = message.Chat.Id;
+
+            /*Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "Привет, путник напиши пожалуйста своё имя");
+            return;
+            */
+
+
+            if (message.Text.ToLower() == "/start")
+            {
+                await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать в нашем баре, добрый путник!");
+                return;
+            }
+                await botClient.SendTextMessageAsync(message.Chat, "Введи имя пожалуйста!");
+                // чё сюда надо?
+                await botClient.SendTextMessageAsync(chatId: chatId, text: "Ну удачного путешествия");
+            
+        }
+        public static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using ConsoleApp3;
+
+
+var botClient = new TelegramBotClient("5309738011:AAGbGOCM9HioJM2Tg4057uYKutT8dZSxbT0");
+
+using var cts = new CancellationTokenSource();  //Создаёт объект для остановки
+
+var receiverOptions = new ReceiverOptions //задаём реакцию,те на какие сообщения реагирует бот
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+botClient.StartReceiving(
+updateHandler: BotHandlers.HandleUpdateAsync,
+pollingErrorHandler: BotHandlers.HandlePollingErrorAsync,
+receiverOptions: receiverOptions,
+cancellationToken: cts.Token
+);
+
+var me = await botClient.GetMeAsync();
+
+Console.WriteLine("Bot started");
+Console.WriteLine($"Start listening for @{me.Username}");
+Console.WriteLine("Press enter for stop");
+Console.ReadKey();
+
+cts.Cancel();
+
+Console.WriteLine("Bot stopped");
